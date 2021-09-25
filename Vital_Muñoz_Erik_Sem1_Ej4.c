b90f2b542e5a3426617751d563dd80d557adf71f
ErikVital <89081103+ErikVital@users.noreply.github.com>
2021-09-25 11:06:56 -0500

    Add files via upload

---
+++ Vital_Muñoz_Erik_Sem1_Ej4.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+
+#define N 5
+#define M 4
+
+void darValor(int mat[M][N]);
+void verMatriz(int mat[M][N]);
+void buscaValor(int mat[M][N], int *pos1, int *pos2,  int *num);
+void Imprime(int uno, int dos, int mayor);
+
+int main()
+{
+	int matriz[M][N];
+	int i, x, y, numero=0;
+	int posicion;
+	
+	darValor(matriz);
+	verMatriz(matriz);
+	buscaValor(matriz, &x, &y, &numero);
+	//posicion=buscaValor(matriz);
+	Imprime(x, y, numero);
+	
+	return 0;
+}
+
+void darValor(int mat[M][N])
+{
+	int i, j;
+	
+	for(i=0; i<M ; i++)
+	{
+		for(j=0; j<N ; j++)
+		{
+			printf("matriz[%d][%d]: ", i, j);
+			scanf("%d", &mat[i][j]);
+		}	
+	}			
+}
+
+void verMatriz(int mat[M][N])
+{
+	int i, j;
+	
+	for(i=0; i<M ; i++)
+	{
+		for(j=0; j<N ; j++)
+		{
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");	
+	}			
+}
+
+void buscaValor(int mat[M][N], int *pos1, int *pos2,  int *num)
+{
+	int i, j;
+	
+	for(i=0; i<M ; i++)
+	{
+		for(j=0; j<N ; j++)
+		{
+			if(*num<mat[i][j])
+			{
+				*num=mat[i][j];
+				*pos1=i;
+				*pos2=j;
+			}
+		}	
+	}	
+}
+
+
+void Imprime(int uno, int dos, int mayor)
+{
+    printf("\nEl numero mayor es %d y se encuentra en la posicion [%d][%d]", mayor, uno, dos);
+}
